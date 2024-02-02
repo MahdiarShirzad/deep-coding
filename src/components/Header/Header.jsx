45ef0bc314b2941baa7e020c0168022eed0d5d26
MahdiarShirzad <101416335+MahdiarShirzad@users.noreply.github.com>
2024-02-02 17:50:09 +0330

    header style debugged

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Logo from "../Logo/Logo";
 import HeaderNav from "./HeaderNav";
 import HeaderLeft from "./HeaderLeft";
 
 const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const scrollThreshold = 50;
+      setIsScrolled(scrollPosition > scrollThreshold);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className=" fixed top-0 right-0 left-0 border-b z-50 bg-white">
+    <div
+      className={`fixed top-0 right-0 left-0 border-b z-50 bg-${
+        isScrolled ? "white" : "transparent"
+      } transition-all`}
+    >
       <div className="container max-w-[1320px] flex mx-auto items-center py-4 font-iransans pr-6">
         <Logo />
         <HeaderNav />
