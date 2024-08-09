991b1b1e5bc2fe5afe797e1e2bb5faad3633b46c
MahdiarShirzad <101416335+MahdiarShirzad@users.noreply.github.com>
2024-08-10 00:58:56 +0330

    .

--- src/screens/Landing/TopCategory.jsx
+++ src/screens/Landing/TopCategory.jsx
@@ -6,6 +6,8 @@ import "aos/dist/aos.css";
 const TopCategory = ({ items, isLoading }) => {
   const uniqueCategories = [...new Set(items.map((item) => item.category))];
 
+  // console.log(uniqueCategories);
+
   useEffect(() => {
     AOS.init({
       duration: 1200, // Specify the animation duration
@@ -24,7 +26,7 @@ const TopCategory = ({ items, isLoading }) => {
     >
       <h3 className="text-2xl font-semibold text-center">دسته بندی های برتر</h3>
       <p className="text-center mt-5">
-        لورم ایپسوم متن ساختگی با تولید سادگی است.
+        دسته‌بندی‌های برتر را کشف کنید و سفر یادگیری خود را آغاز کنید.{" "}
       </p>
       <div>
         <TopCategoriesSlider uniqueCategories={uniqueCategories} />
